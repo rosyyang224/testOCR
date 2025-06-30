a3a9a4af48da11732115928dcf51b64a040fdaaa
Rosemary Yang <yang.rosemary.e@gmail.com>
2025-06-30 18:02:33 +0800

    added key-value table scrolling and fixed show/hide button

--- airbank-ocr-demo/RecognizedKeyValue.swift
+++ airbank-ocr-demo/RecognizedKeyValue.swift
@@ -1,26 +1,45 @@
-//import UIKit
 //
-//@UIApplicationMain
-//class MainAppDelegate: UIResponder, UIApplicationDelegate {
-//    func application(
-//        _ application: UIApplication,
-//        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-//    ) -> Bool {
-//        return true
-//    }
+//  RecognizedKeyValue.swift
+//  airbank-ocr-demo
 //
-//    func application(
-//        _ application: UIApplication,
-//        configurationForConnecting connectingSceneSession: UISceneSession,
-//        options: UIScene.ConnectionOptions
-//    ) -> UISceneConfiguration {
-//        return UISceneConfiguration(name: "Main Configuration", sessionRole: connectingSceneSession.role)
-//    }
+//  Created by Marek Přidal on 20/11/2019.
+//  Copyright © 2019 Marek Přidal. All rights reserved.
 //
-//    func application(
-//        _ application: UIApplication,
-//        didDiscardSceneSessions sceneSessions: Set<UISceneSession>
-//    ) {
-//        // No resources to clean up in this case
-//    }
-//}
+
+import Foundation
+import Vision
+
+struct RecognizedKeyValue {
+    enum Alignment {
+        case vertical
+        case horizontal
+    }
+    
+    enum DocumentElement: String, CaseIterable {
+        case surname = "SURNAME"
+        case givenNames = "GIVEN NAMES"
+        case dateOfBirth = "DATE OF BIRTH"
+        case documentNo = "DOCUMENT NO."
+        case placeOfBirth = "PLACE OF BIRTH"
+        case nationality = "NATIONALITY"
+        case dateOfIssue = "DATE OF ISSUE"
+        case dateOfExpiry = "DATE OF EXPIRY"
+        case sex = "SEX"
+    }
+    
+    let key: String
+    let keyTextObservation: VNRecognizedTextObservation
+    
+    var keyPosition: VNRectangleObservation? {
+        try? keyTextObservation.topCandidates(10).first(where: { $0.string == key })?.boundingBox(for: Range<String.Index>.init(uncheckedBounds: (key.startIndex, key.endIndex)))
+    }
+    var alignment: Alignment {
+        key.contains("SURNAME") || key.contains("GIVEN NAMES") || key.contains("DOCUMENT NO.") ? .horizontal : .vertical
+    }
+    var documentElement: DocumentElement? {
+        DocumentElement(rawValue: key)
+    }
+
+    var value: String?
+    var valueTextObservation: VNRecognizedTextObservation?
+}
