35fa1af6b99480c02113502bd403a1b0ac80b1cf
Rosemary Yang <yang.rosemary.e@gmail.com>
2025-06-30 15:16:05 +0800

    got rid of live functionality

--- airbank-ocr-demo/PreviewView.swift
+++ airbank-ocr-demo/PreviewView.swift
@@ -8,93 +8,26 @@
 
 import UIKit
 import Vision
-import AVFoundation
 
 class PreviewView: UIView {
-    
     private var maskLayer = [CAShapeLayer]()
-    
-    // MARK: AV capture properties
-    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
-        return layer as! AVCaptureVideoPreviewLayer
-    }
-    
-    var session: AVCaptureSession? {
-        get {
-            return videoPreviewLayer.session
-        }
-        
-        set {
-            videoPreviewLayer.session = newValue
-        }
-    }
-    
-    override class var layerClass: AnyClass {
-        return AVCaptureVideoPreviewLayer.self
-    }
-    
-    // Create a new layer drawing the bounding box
-    private func createLayer(in rect: CGRect) -> CAShapeLayer{
-        
+
+    func drawRect(textObservation: VNRecognizedTextObservation) {
+        let transform = CGAffineTransform(scaleX: 1, y: -1).translatedBy(x: 0, y: -frame.height)
+        let translate = CGAffineTransform.identity.scaledBy(x: frame.width, y: frame.height)
+        let textBounds = textObservation.boundingBox.applying(translate).applying(transform)
+
         let mask = CAShapeLayer()
-        mask.frame = rect
+        mask.frame = textBounds
         mask.cornerRadius = 10
         mask.opacity = 0.75
         mask.borderColor = UIColor.yellow.cgColor
         mask.borderWidth = 2.0
-        
+
         maskLayer.append(mask)
         layer.insertSublayer(mask, at: 1)
-        
-        return mask
-    }
-    
-    func drawFaceboundingBox(face : VNFaceObservation) {
-        
-        let transform = CGAffineTransform(scaleX: 1, y: -1).translatedBy(x: 0, y: -frame.height)
-        
-        let translate = CGAffineTransform.identity.scaledBy(x: frame.width, y: frame.height)
-        
-        // The coordinates are normalized to the dimensions of the processed image, with the origin at the image's lower-left corner.
-        let facebounds = face.boundingBox.applying(translate).applying(transform)
-        
-        _ = createLayer(in: facebounds)
-        
-    }
-    
-    private func drawPointsOnLayer(rect:CGRect, landmarkPoints: [CGPoint], isClosed: Bool = true) -> CALayer {
-        let linePath = UIBezierPath()
-        linePath.move(to: landmarkPoints.first!)
-        
-        for point in landmarkPoints.dropFirst() {
-            linePath.addLine(to: point)
-        }
-        
-        if isClosed {
-            linePath.addLine(to: landmarkPoints.first!)
-        }
-        
-        let lineLayer = CAShapeLayer()
-        lineLayer.path = linePath.cgPath
-        lineLayer.fillColor = nil
-        lineLayer.opacity = 1.0
-        lineLayer.strokeColor = UIColor.green.cgColor
-        lineLayer.lineWidth = 0.02
-        
-        return lineLayer
-    }
-    
-    func drawRect(textObservation: VNRecognizedTextObservation) {
-        let transform = CGAffineTransform(scaleX: 1, y: -1).translatedBy(x: 0, y: -frame.height)
-        
-        let translate = CGAffineTransform.identity.scaledBy(x: frame.width, y: frame.height)
-        
-        // The coordinates are normalized to the dimensions of the processed image, with the origin at the image's lower-left corner.
-        let textBounds = textObservation.boundingBox.applying(translate).applying(transform)
-        
-        let layer = createLayer(in: textBounds)
-        
-        let label = UILabel(frame: layer.frame)
+
+        let label = UILabel(frame: mask.frame)
         label.font = .systemFont(ofSize: 5)
         label.textColor = .systemRed
         if let topResult = textObservation.topCandidates(1).first {
@@ -102,7 +35,7 @@ class PreviewView: UIView {
         }
         addSubview(label)
     }
-    
+
     func removeMask() {
         for mask in maskLayer {
             mask.removeFromSuperlayer()
@@ -110,5 +43,4 @@ class PreviewView: UIView {
         subviews.forEach({ $0.removeFromSuperview() })
         maskLayer.removeAll()
     }
-    
 }
