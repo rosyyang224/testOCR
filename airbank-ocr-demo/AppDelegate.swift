d09ed40bed390dc4218e6674c6b0ea4488c3fedb
Rosemary Yang <yang.rosemary.e@gmail.com>
2025-07-01 17:01:26 +0800

    added entry point:

---
+++ airbank-ocr-demo/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  airbank-ocr-demo
+//
+//  Created by Rosemary Yang on 7/1/25.
+//  Copyright © 2025 Marek Přidal. All rights reserved.
+//
+
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Handle cleanup here if needed
+    }
+}
