7f8bc224056fe6a382b59bd4e386ad24b0b040f6
Rosemary Yang <yang.rosemary.e@gmail.com>
2025-07-01 16:58:05 +0800

    working

---
+++ airbank-ocr-demo/WindowSceneCoordinator.swift
@@ -0,0 +1,33 @@
+//
+//  WindowSceneCoordinator.swift
+//  airbank-ocr-demo
+//
+//  Created by Rosemary Yang on 7/1/25.
+//  Copyright © 2025 Marek Přidal. All rights reserved.
+//
+
+
+import UIKit
+
+class WindowSceneCoordinator: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+
+        window = UIWindow(windowScene: windowScene)
+        let rootVC = ImageUploadController()
+        let navController = UINavigationController(rootViewController: rootVC)
+        navController.tabBarItem = UITabBarItem(title: "Upload", image: UIImage(systemName: "photo"), selectedImage: UIImage(systemName: "photo.fill"))
+
+        let tabController = UITabBarController()
+        tabController.viewControllers = [navController]
+
+        window?.rootViewController = tabController
+        window?.makeKeyAndVisible()
+    }
+}
