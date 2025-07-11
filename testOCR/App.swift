0a402a313d12550ebbace67592745560f767cc9c
Rosemary Yang <yang.rosemary.e@gmail.com>
2025-07-11 10:12:40 +0800

    removed pythonkit dependencies

--- testOCR/App.swift
+++ testOCR/App.swift
@@ -1,15 +1,8 @@
 import SwiftUI
-import PythonKit
 
 @main
 struct MyApp: App {
     init() {
-        PythonLibrary.useLibrary(at: "/opt/miniconda3/envs/swiftpy/lib/libpython3.12.dylib")
-        let sys = Python.import("sys")
-        print("PythonKit initialized")
-        print("sys.version =", sys.version)
-        print("sys.executable =", sys.executable)
-        
         AppTheme()
     }
     
